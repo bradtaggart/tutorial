37850eab70dadd2763d9aa85d2e3d5d2eb362efd
Brad Taggart <brad@sbthome.com>
2020-12-03 15:49:43 -0700

    test

---
+++ src/books.js
@@ -0,0 +1,22 @@
+const books = [
+  {
+    id: 1,
+    title: 'Ready Player Two: A Novel',
+    author: 'Ernest Cline',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/811W9hHXiwL._AC_UL210_SR210,210_.jpg'
+  },
+  {
+    id: 2,
+    title: 'A Promised Land',
+    author: 'Barack Obama',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg'
+   },
+  {
+    id: 3,
+    title: 'Greenlights',
+    author: 'Matthew McConaughey',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81Kc8OsbDxL._AC_UL200_SR200,200_.jpg'
+  }
+]
+
+export default books
